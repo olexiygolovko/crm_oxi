ef90dbd09ca373e0b87b51762668ce23f338c7b4
olexiygolovko <olexiygolvoko92@mai.ru>
2024-09-25 22:12:15 +0300

    created requirements.txt

--- crm_oxi/task_manager/forms.py
+++ crm_oxi/task_manager/forms.py
@@ -10,4 +10,15 @@ class TaskForm(forms.ModelForm):
         fields = ['title', 'description', 'status', 'due_date']
         widgets = {
             'due_date': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'description': forms.Textarea(attrs={
+                'placeholder': 'Enter task description here...',
+                'rows': 3 
+            }),
+            'title': forms.TextInput(attrs={
+                'placeholder': 'Enter task title here...',
+            }),
         }
+    def __init__(self, *args, **kwargs):
+        super(TaskForm, self).__init__(*args, **kwargs)
+        # Remove the labels for the fields
+        self.fields['description'].label = ''
