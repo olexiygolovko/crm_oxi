53625da9a7b3da53d863947d4f01078e0a43342d
olexiygolovko <olexiygolvoko92@mai.ru>
2024-09-25 22:51:39 +0300

    testing settings

--- crm_oxi/crm_oxi/settings.py
+++ crm_oxi/crm_oxi/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'crm',
     'django_filters',
     'task_manager',
-    
+
 ]
 
 MIDDLEWARE = [
