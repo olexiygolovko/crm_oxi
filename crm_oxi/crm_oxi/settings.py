85e82d94713113a631f84972954cc17b4c83b588
olexiygolovko <olexiygolvoko92@mai.ru>
2024-09-26 22:20:35 +0300

    update settings

--- crm_oxi/crm_oxi/settings.py
+++ crm_oxi/crm_oxi/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['crmoxi.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
