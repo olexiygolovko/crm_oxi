e2bf2ed26e6b9f988bcc5778bb69736ac82c3c20
olexiygolovko <olexiygolvoko92@mai.ru>
2024-12-01 23:50:10 +0300

    update settings

--- crm_oxi/crm_oxi/wsgi.py
+++ crm_oxi/crm_oxi/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+sys.path.append('/opt/render/project/src')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crm_oxi.settings')
 
 application = get_wsgi_application()
