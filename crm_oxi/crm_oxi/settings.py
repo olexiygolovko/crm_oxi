a16215e5eb696251ac926fe3da55d85c40063dfb
olexiygolovko <olexiygolvoko92@mai.ru>
2024-12-01 14:51:37 +0300

    added procfile

--- crm_oxi/crm_oxi/settings.py
+++ crm_oxi/crm_oxi/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'https://crm-oxi.onrender.com']
 
 
 # Application definition
